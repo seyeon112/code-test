3666f6b682f568b3e97a09d1893d32c833842477
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-01-16 23:34:43 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -3,4 +3,22 @@ package levelZero;
 public class Test1 {
 
 	
+//알고리즘
+//switch문 사용
+int solution(int angle) {
+    if (0 < angle && angle <= 180) {
+        int answer = 0;
+       if(0 < angle && angle < 90) {
+           answer = 1;
+       }else if(angle == 90) {
+           answer = 2;
+       }else if(90 < angle && angle < 180) {
+           answer = 3;
+       }else{
+           answer = 4;
+       }
+        return answer;
+    }
+}
+	
 }
