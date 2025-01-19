20fe5669d3ffcdda8f4740dd1957f3f23efbea8e
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-01-19 18:33:27 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -41,5 +41,12 @@ int solution(int num1, int num2) {
     }
  
 }
+
+int solution(int num1, int num2) {
+    if((0 < num1 && num1 <= 100)&&(0 < num2 && num2 <= 100)) {
+        int answer = num1 / num2;
+        return answer;
+    }
+}
 	
 }
