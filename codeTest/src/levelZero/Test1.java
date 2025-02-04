3d3ec3fbd16e037e786dde2ddae737fea4cd679a
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-04 22:10:43 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -120,5 +120,16 @@ double solution(int numbers[], size_t numbers_len) {
     
     return answer;
 }
+
+// array_len은 배열 array의 길이입니다.
+int solution(int array[], size_t array_len, int height) {
+    int answer = 0;
+    for(int i = 0; i < array_len; i++) {
+        if(array[i] > height) {
+            answer++;
+        }
+    }
+    return answer;
+}
 	
 }
