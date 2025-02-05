25641249ab092b14d3cf95cf0f2017433405ef9b
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-05 12:04:56 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -131,5 +131,20 @@ int solution(int array[], size_t array_len, int height) {
     }
     return answer;
 }
+
+int solution(int n) {
+    int answer = 2;
+
+    for (int i = 1 ; i * i <= n ; i++)
+    {
+        if (i * i == n)
+        {
+            answer = 1;
+            break;
+        }
+    }
+
+    return answer;
+}
 	
 }
