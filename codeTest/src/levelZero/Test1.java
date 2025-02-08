8e1342335e31fc14a86b70c63dfe736bb8595def
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-08 10:26:47 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -160,5 +160,15 @@ int solution(int n) {
 
     return answer;
 }
+
+import java.util.stream.IntStream;
+class Solution {
+    public int[] solution(int n) {
+
+        return IntStream.rangeClosed(0, n)
+            .filter(e -> e % 2 != 0)
+            .toArray();
+    }
+}
 	
 }
