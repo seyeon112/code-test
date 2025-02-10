e2f9aaa5869f7b7161c38036b276f079768d75c1
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-10 10:51:31 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -183,5 +183,14 @@ class Solution {
         return answer;
     }
 }
+
+class Solution {
+    public int solution(String str1, String str2) {
+        if (str1.contains(str2)) {
+            return 1;
+        }
+        return 2;
+    }
+}
 	
 }
