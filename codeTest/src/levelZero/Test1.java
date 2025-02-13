6f3df911e926c5936bd0431cd7e550d6cc901efd
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-13 17:26:41 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -215,5 +215,19 @@ class Solution {
         return answer;
     }
 }
+
+class Solution {
+    public int solution(int[] numbers) {
+        int answer = 0;
+        for (int i=0; i<numbers.length; i++) {
+            for (int j=0; j<numbers.length; j++) {
+                if (i != j) {
+                    answer = Math.max(answer, numbers[i] * numbers[j]);
+                }
+            }
+        }
+        return answer;
+    }
+}
 	
 }
