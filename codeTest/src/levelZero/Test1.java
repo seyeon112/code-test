e39ddeca7db118e61e568eb371f9f924a0724eee
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-26 11:29:03 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -47,6 +47,15 @@ int solution(int num1, int num2) {
     return answer;
 }
 
+
+int solution(int num1, int num2) {
+    int answer = -1;
+    if ((0 < num1 && num1 <= 100)&&(0 < num2 && num2 <= 100)) {
+        answer = num1 % num2;
+    }
+    return answer;
+}
+	
 int solution(int num1, int num2) {
     if((num1 >= -50000)&&(num2 >= -50000)) {
         int answer = num1 - num2;
