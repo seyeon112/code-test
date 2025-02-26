6f4d8d87ddfaabd15b631cb410ebe67116f70a71
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-02-26 11:13:19 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -40,6 +40,14 @@ int solution(int age) {
 }
 
 int solution(int num1, int num2) {
+    int answer = 0;
+    if ((-50000 <= num1 && num1 <= 50000)&&(-50000 <= num2 && num2 <= 50000)) {
+        answer = num1 - num2;
+    }
+    return answer;
+}
+
+int solution(int num1, int num2) {
     if((num1 >= -50000)&&(num2 >= -50000)) {
         int answer = num1 - num2;
         return answer;
