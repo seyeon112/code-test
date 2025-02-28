275ad5e79841a29d94c7846541d99777de159ad8
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-01 08:25:50 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -394,5 +394,17 @@ class Solution {
         return answer;
     }
 }
+
+// 더 간단하게
+class Solution {
+    public int[] solution(int money) {
+        int[] answer = {0, 0};
+        answer[0] = money / 5500;
+        answer[1] = money % 5500;
+        return answer;
+    }
+}
+
+
 	
 }
