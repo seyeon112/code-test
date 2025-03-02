b2390e2ef0c2fe52417b36f4fe47b10a56787128
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-02 11:36:26 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -417,6 +417,19 @@ class Solution {
     }
 }
 
+class Solution {
+    public int[] solution(String[] strlist) {
+        int[] answer = new int[strlist.length];
+        if(1 <= strlist.length && strlist.length <= 100) {
+            for(int i = 0; i < answer.length; i++) {
+                answer[i] = strlist[i].length();
+            }
+            
+        }
+        return answer;
+    }
+}
+
 
 	
 }
