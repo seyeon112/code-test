7ba9ce9ac2e1fe9b985dd4ad22e0d279313968f7
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-03 10:49:00 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -428,6 +428,22 @@ class Solution {
         }
         return answer;
     }
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        int[] answer = new int[2];
+        if((1 <= num_list.length && num_list.length <= 100) && (0 <= num_list.length && num_list.length <= 1000)) {
+            for (int i = 0; i <= num_list.length; i++) {
+                 if(num_list[i] % 2 == 0) {
+                    answer[0]++;
+                } else {
+                    answer[1]++;
+                }
+            } 
+        }
+        return answer;
+    }
+}
 }
 
 
