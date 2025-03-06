0c39638706bc4195222ad18dc8f1dd88fbff67a6
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-06 09:54:14 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -458,6 +458,22 @@ class Solution {
     }
 }
 
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }   
+    }
+}
+
 }
 
 
