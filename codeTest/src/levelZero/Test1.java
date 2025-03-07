af752a6a2f30db3a4c51e57efed3c5bc49501f87
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-07 22:39:54 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -474,6 +474,27 @@ public class Solution {
     }
 }
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+     for(int i=0;i<=1000;i++)
+    {
+        if(i*i==n)
+        {
+            answer=1;
+            break;
+        }
+        else
+        {
+            answer=2;
+        }
+    }
+    return answer;
+}
+
 }
 
 
