ba55ae03eb4497878457829c1dc45293a07061ac
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-08 17:28:55 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -495,7 +495,18 @@ int solution(int n) {
     return answer;
 }
 
+class Solution {
+    public int solution(String message) {
+        int answer = 0;
+        if (1 <= message.length() && message.length() <= 50) {
+            answer += message.length() * 2;
+        }
+        return answer;
+    }
 }
+
+}
+
 
 
 	
