8b55acb6c62df90963ef7ac942b06b4918026e08
seyeon112 <161185949+seyeon112@users.noreply.github.com>
2025-03-16 23:42:37 +0900

    Update Test1.java

--- codeTest/src/levelZero/Test1.java
+++ codeTest/src/levelZero/Test1.java
@@ -589,6 +589,16 @@ int solution(int slice, int n) {
     }
     return answer;
 }
+
+function solution(names) {
+    let answer = [];
+    
+    for (let i = 0; i < names.length; i+=5) {
+        answer.push(names[i]);
+    }
+    
+    return answer;
+}
 }
 
 
